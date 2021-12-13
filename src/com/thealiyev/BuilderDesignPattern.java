ac1adaa30b8e201147bacdcc528414bde7d13b67
aliyevroyal <royal.alyv@gmail.com>
2021-12-13 14:11:09 +0400

    Initial commit

---
+++ src/com/thealiyev/BuilderDesignPattern.java
@@ -0,0 +1,9 @@
+package com.thealiyev;
+
+public class BuilderDesignPattern {
+    public static class Main {
+        public static void main(String args[]) {
+            System.out.println("Hello Builder Design Pattern");
+        }
+    }
+}
